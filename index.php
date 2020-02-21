c25b2a468b5caf320c8b739edfc240cbfab9b39c
joerilapin <joerilapin>
2020-02-21 14:20:32 +0100

    test

--- index.php
+++ index.php
@@ -1,53 +1,14 @@
 <?php
-
-
-
-//hier komen de requires
-
-
-declare(strict_types=1);
-
-//include all your model files here
-
-//include all your controllers here
-require 'Controller/homepagecontroller.php';
-
-//you could write a simple IF here based on some $_GET or $_POST vars, to choose your controller
-//this file should never be more than 20 lines of code!
-
-
-
-
-<!--//file_put_contents ( string $filename , mixed $data [, int $flags = 0 [, resource $context ]] ) : int
-
-
-
-/*$file = 'guestbook.txt';
-// Open the file to get existing content
-$current = file_get_contents($file);
-// Append a new person to the file
-$current1 = "Joeri Lapin1\n";
-//$current2 = "Joeri Lapin2\n";
-// Write the contents back to the file
-file_put_contents($file, $current1);
-//file_put_contents($file, $current2);
-echo($current1);
-//echo($current2);*/
-
 declare(strict_types=1);
 
 ini_set('display_errors', "1");
 ini_set('display_startup_errors', "1");
 error_reporting(E_ALL);
 
-/*echo 'test';
-$control= new HomeController
-$control->render();*/
-
 require 'Controller/homepagecontroller.php';
-require 'Model/guestpookpost';
-
-session_start();
+echo 'test';
+$control= new HomeController;
+$control->render();
 
 
 
